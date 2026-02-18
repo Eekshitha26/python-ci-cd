56875ea6a58703dc6009aaf5e9b3ec34e891fc76
ayushshakya-ca <ayush.shakya@cloudambassadors.com>
2026-02-18 12:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def home():
     return render_template("index.html", launch_date=LAUNCH_DATE)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
